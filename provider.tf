d8f62733c3d044f7ac37f314052f032a63640399
EC2 Default User <ec2-user@ip-172-31-10-10.us-west-2.compute.internal>
2024-08-06 08:13:00 +0000

    add

--- provider.tf
+++ provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.61.0"
+    }
+  }
+}
+
 provider "aws" {
-    region = "us-west-2"
+  region = "us-west-2"
 }
